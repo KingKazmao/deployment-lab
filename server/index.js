d421e5e7199544282f1ac30765d2f0ab38e428cb
Brandon Laubaugh <kingkazma133@gmail.com>
2021-06-24 16:14:18 -0400

    ayeee

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+app.get('/', (req, res) => {
+
+    res.sendFile( path.join(__dirname, '../index.html') );
+    res.sendFile( path.join(__dirname, '../styles.css') );
+
+})
+
+
+
+const port = process.env.PORT || 5050;
+
+app.listen(port, () => {
+    console.log(`Server breathing on ${port}`)
+})
+
